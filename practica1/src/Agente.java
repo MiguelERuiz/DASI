ce8c792b7ee997bce3985e52ebdf44ff706cf151
Miguel E. Ruiz <meruiz77@gmail.com>
2022-02-24 18:14:04 +0100

    Cambios en el agente minimo y ejecucion

--- practica1/src/Agente.java
+++ practica1/src/Agente.java
@@ -3,10 +3,9 @@ import jade.core.Agent;
 public class Agente extends Agent {
     protected void setup() {
         System.out.println("Hola! Soy el AgenteMinimo " + getAID().getName());
-        doDelete();
     }
 
     protected void takeDown() {
-        System.out.println("Bye me fui");
+        System.out.println("Bye Bye cruel world!");
     }
 }
